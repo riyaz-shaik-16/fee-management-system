a64232b99dce59bee6db363323e64e3bbf201806
Riyaz <sr308379@gmail.com>
2025-07-18 22:54:31 +0530

    fix: update SameSite attribute for cookies to 'none' for cross-site requests

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -96,7 +96,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
