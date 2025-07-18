176effe1487b2c05384b25da9850c7f79439af00
Riyaz <sr308379@gmail.com>
2025-07-18 22:51:24 +0530

    feat: update socket connection URL to use production server

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:9000", {
+const socket = io("https://fee-management-system.onrender.com", {
   withCredentials: true,
   autoConnect: false,
 });
